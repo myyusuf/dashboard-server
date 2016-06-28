5d6c14b743368c86502a1b45ba33d0a9e03f47ee
myyusuf <myyusuf1911@gmail.com>
2016-06-28 14:30:58 +0700

    Completion drilldown net profit pph final

--- app/handlers/drilldowndata.js
+++ app/handlers/drilldowndata.js
@@ -16,9 +16,18 @@ exports.kontrakDihadapi = function(req, res, db) {
   var _jsonTotalKontrakDihadapi = {};
   var _jsonSisaKontrakDihadapi = {};
   var _jsonPesananBaruKontrakDihadapi = {};
+
   var _jsonTotalPenjualan = {};
   var _jsonPenjualanLama = {};
   var _jsonPenjualanBaru = {};
+
+  var _jsonTotalLabaKotor = {};
+  var _jsonLabaKotorLama = {};
+  var _jsonLabaKotorBaru = {};
+
+  var _jsonTotalPphFinal = {};
+  var _jsonPphFinalLama = {};
+  var _jsonPphFinalBaru = {};
 
   var _result = {
     month: _month,
@@ -134,6 +143,116 @@ exports.kontrakDihadapi = function(req, res, db) {
     );
   };
 
+//-----
+
+  var _getTotalLabaKotor = function(callback){
+    var _query = "SELECT * FROM db_mobile_total_laba_kotor WHERE tahun=? and bulan=?";
+
+    db.query(
+      _query, [_year, _month],
+      function(err, rows) {
+        if (err) throw err;
+
+        if (rows.length > 0) {
+          var _row = rows[0];
+          _jsonTotalLabaKotor = JSON.parse(_row.data).totalLabaKotor;
+        }
+
+        callback();
+      }
+    );
+  };
+
+  var _getLabaKotorLama = function(callback){
+    var _query = "SELECT * FROM db_mobile_laba_kotor_lama WHERE tahun=? and bulan=?";
+
+    db.query(
+      _query, [_year, _month],
+      function(err, rows) {
+        if (err) throw err;
+
+        if (rows.length > 0) {
+          var _row = rows[0];
+          _jsonLabaKotorLama = JSON.parse(_row.data).labaKotorLama;
+        }
+
+        callback();
+      }
+    );
+  };
+
+  var _getLabaKotorBaru = function(callback){
+    var _query = "SELECT * FROM db_mobile_laba_kotor_baru WHERE tahun=? and bulan=?";
+
+    db.query(
+      _query, [_year, _month],
+      function(err, rows) {
+        if (err) throw err;
+
+        if (rows.length > 0) {
+          var _row = rows[0];
+          _jsonLabaKotorBaru = JSON.parse(_row.data).labaKotorBaru;
+        }
+
+        callback();
+      }
+    );
+  };
+//-----
+  var _getTotalPphFinal = function(callback){
+    var _query = "SELECT * FROM db_mobile_total_pph_final WHERE tahun=? and bulan=?";
+
+    db.query(
+      _query, [_year, _month],
+      function(err, rows) {
+        if (err) throw err;
+
+        if (rows.length > 0) {
+          var _row = rows[0];
+          _jsonTotalPphFinal = JSON.parse(_row.data).totalPphFinal;
+        }
+
+        callback();
+      }
+    );
+  };
+
+  var _getPphFinalLama = function(callback){
+    var _query = "SELECT * FROM db_mobile_pph_final_lama WHERE tahun=? and bulan=?";
+
+    db.query(
+      _query, [_year, _month],
+      function(err, rows) {
+        if (err) throw err;
+
+        if (rows.length > 0) {
+          var _row = rows[0];
+          _jsonPphFinalLama = JSON.parse(_row.data).pphFinalLama;
+        }
+
+        callback();
+      }
+    );
+  };
+
+  var _getPphFinalBaru = function(callback){
+    var _query = "SELECT * FROM db_mobile_pph_final_baru WHERE tahun=? and bulan=?";
+
+    db.query(
+      _query, [_year, _month],
+      function(err, rows) {
+        if (err) throw err;
+
+        if (rows.length > 0) {
+          var _row = rows[0];
+          _jsonPphFinalBaru = JSON.parse(_row.data).pphFinalBaru;
+        }
+
+        callback();
+      }
+    );
+  };
+
   flow.series([
       function (callback) {
           _getTotalKontrakDihadapi(callback);
@@ -154,12 +273,39 @@ exports.kontrakDihadapi = function(req, res, db) {
           _getPenjualanBaru(callback);
       },
       function (callback) {
+          _getTotalLabaKotor(callback);
+      },
+      function (callback) {
+          _getLabaKotorLama(callback);
+      },
+      function (callback) {
+          _getLabaKotorBaru(callback);
+      },
+      function (callback) {
+          _getTotalPphFinal(callback);
+      },
+      function (callback) {
+          _getPphFinalLama(callback);
+      },
+      function (callback) {
+          _getPphFinalBaru(callback);
+      },
+      function (callback) {
           _result.jsonData['totalKontrakDihadapi'] = _jsonTotalKontrakDihadapi;
           _result.jsonData['sisaKontrakDihadapi'] = _jsonSisaKontrakDihadapi;
           _result.jsonData['pesananBaruKontrakDihadapi'] = _jsonPesananBaruKontrakDihadapi;
+
           _result.jsonData['totalPenjualan'] = _jsonTotalPenjualan;
           _result.jsonData['penjualanLama'] = _jsonPenjualanLama;
           _result.jsonData['penjualanBaru'] = _jsonPenjualanBaru;
+
+          _result.jsonData['totalLabaKotor'] = _jsonTotalLabaKotor;
+          _result.jsonData['labaKotorLama'] = _jsonLabaKotorLama;
+          _result.jsonData['labaKotorBaru'] = _jsonLabaKotorBaru;
+
+          _result.jsonData['totalPphFinal'] = _jsonTotalPphFinal;
+          _result.jsonData['pphFinalLama'] = _jsonPphFinalLama;
+          _result.jsonData['pphFinalBaru'] = _jsonPphFinalBaru;
           res.json(_result);
       }
   ]);
